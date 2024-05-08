508ecbf78b2548b3463c3ed2589636d4e6f0a328
kevin168520 <150585528+kevin168520@users.noreply.github.com>
2024-05-08 21:44:38 +0800

    Update 血量系統

---
+++ Assets/Scripts/HpEnemy.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace Kevin
+{
+    /// <summary>
+    /// 敵人血量系統
+    /// </summary>
+    public class HpEnemy : HpSystem
+    {
+        [SerializeField, Header("敵人資料")]
+        private DataEnemy dataEnemy;
+
+        private string weaponName = "武器";
+
+        private void Awake()
+        {
+            hp = dataEnemy.hp;
+        }
+
+        // OnCollisionXXX
+        // 條件:兩個物件都有碰撞器並且都沒有勾選 IsTrigger
+
+        // OnTriggerXXX
+        // 條件:兩個物件都有碰撞器並且其中一個勾選 IsTrigger
+
+        // OnCollisionEnter 碰撞開始執行一次
+        // OnCollisionStay 碰撞時持續執行:約60FPS
+        // OnCollisionExit 碰撞結束執行一次
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            //print($"<color=#f63>碰到的物件:{collision.gameObject.name}</color>");
+
+            // 如果碰到的物件名稱有"武器"就受傷
+            if (collision.gameObject.name.Contains(weaponName))
+            {
+                Damage(30);
+            }
+
+        }
+
+        // 使用關鍵字 override 可以覆寫父類別有 virtual 的成員
+        protected override void Dead()
+        {
+            // base 父類別原有的內容，可刪除
+            base.Dead();
+
+            print("<color=#f66>生成爆炸特效</color>");
+
+            // 刪除此物件
+            Destroy(gameObject);
+        }
+    }
+
+}
---
+++ Assets/Scripts/HpSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Kevin
+{
+    /// <summary>
+    /// 血量系統
+    /// </summary>
+    public class HpSystem : MonoBehaviour
+    {
+        [SerializeField, Header("畫布傷害值欲置物")]
+        private GameObject prefabDamageCanvas;
+
+        protected float hp;
+
+        /// <summary>
+        /// 受傷
+        /// </summary>
+        /// <param name="damage"></param>
+        protected void Damage(float damage)
+        {
+            Instantiate(prefabDamageCanvas, transform.position,Quaternion.identity);
+            hp -= damage;
+            print($"<color=#f33>{gameObject.name}受傷，血量剩下:{hp}</color>");
+
+            // 如果血量<=0死亡
+            if (hp <= 0) Dead();
+        }
+
+        // virtual 虛擬關鍵字: 允許使用子類別使用關鍵字 override 覆寫
+        /// <summary>
+        /// 死亡
+        /// </summary>
+        protected virtual void Dead()
+        {
+            print($"<color=#f33>{gameObject.name}死亡</color>");
+        }
+    }
+}
+
