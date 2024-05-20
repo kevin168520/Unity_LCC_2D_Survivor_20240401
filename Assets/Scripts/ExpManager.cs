a73558098f7d726eb6eb1d3d1c87fb8cf40cb2c2
kevin168520 <150585528+kevin168520@users.noreply.github.com>
2024-05-20 10:06:13 +0800

    Update 經驗值

---
+++ Assets/Scripts/ExpManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+
+namespace Kevin
+{
+    /// <summary>
+    /// 經驗值管理器
+    /// </summary>
+    public class ExpManager : MonoBehaviour
+    {
+        /// <summary>
+        /// 玩家目前的經驗值
+        /// </summary>
+        private float expCurrent;
+        /// <summary>
+        /// 經驗值需求:需要多少才會升級
+        /// </summary>
+        private float expNeed;
+        /// <summary>
+        /// 等級
+        /// </summary>
+        private int lv = 1;
+        /// <summary>
+        /// 等級最大值
+        /// </summary>
+        private int lvMax = 100;
+        /// <summary>
+        /// 經驗值需求表格:所有等級經驗值需求
+        /// </summary>
+        [SerializeField]
+        private float[] expNeedTable;
+
+        [ContextMenu("產生經驗值需求表格")]
+        private void GeneratedExpNeedTable()
+        {
+            // 指定陣列的數量危等級最大值
+            expNeedTable = new float[lvMax];
+
+            for (int i = 0; i < lvMax; i++)
+            {
+                // 經驗值公式 等級需求為 等級 * 100
+                expNeedTable[i] = (i + 1) * 100;
+            }
+        }
+    }
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
 
 namespace Kevin
 {
@@ -7,6 +10,7 @@ namespace Kevin
     /// </summary>
     public class GameManager : MonoBehaviour
     {
+        #region 常數資料
         // const 常數 : 不變的資料
         public const string playerName = "玩家_巫師";
         /// <summary>
@@ -17,6 +21,44 @@ namespace Kevin
         /// 動畫控制器參數 : 觸發攻擊
         /// </summary>
         public const string parAttack = "觸發攻擊";
+        #endregion
+
+        #region 一般資料
+        [SerializeField, Header("玩家血量系統")]
+        private HpPlayer hpPlayer;
+        [SerializeField, Header("畫布結束畫面")]
+        private CanvasGroup groupFinal; 
+        #endregion
+
+        private void Awake()
+        {
+            hpPlayer.onDead += ShowFinalCanvas; 
+        }
+
+        /// <summary>
+        /// 顯示結束畫面
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        private void ShowFinalCanvas(object sender, EventArgs e)
+        {
+            StartCoroutine(FadeCanvas());
+        }
+
+        private IEnumerator FadeCanvas()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                // 透明度 遞增 0.1
+                groupFinal.alpha += 0.1f;
+                yield return new WaitForSeconds(0.05f);
+            }
+
+            // 畫布群組勾選互動 與 射線遮擋
+            groupFinal.interactable = true;
+            groupFinal.blocksRaycasts = true;
+        }
     }
 }
 
